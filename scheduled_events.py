3807ed85351f232c7b0c6630ef0f3e3cfb62ee5a
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2026-04-04 13:43:48 +0200

    added cancel_events which does the same as fetch but without time constraint

--- scheduled_events.py
+++ scheduled_events.py
@@ -49,6 +49,48 @@ class ScheduledEvent:
         BotState.write()
 
     @staticmethod
+    def cancel_events(event_type: str, chat_id:int = 0, filters=None):
+        """
+
+        @param event_type:
+        @param chat_id:
+        @param filters:
+        @return:
+        """
+        get_q = """
+        SELECT chatid, time, data0, data1, data2, data3, data4, data5, data6, data7
+        FROM scheduled_events
+        WHERE etype == ?
+        """
+        del_q = """
+        DELETE
+        FROM scheduled_events
+        WHERE etype == ?"""
+        if filters is None:
+            return
+        filter_list = []
+        q_list = [event_type]
+        if chat_id != 0:
+            filter_list.append(("chatid",chat_id))
+        for col, val in filters:
+            if 8 > col > 0:
+                continue
+            filter_list.append(("data" + str(col),val))
+        for q_filter in filter_list:
+            get_q = get_q + " AND " + q_filter[0] + " = ? "
+            del_q = del_q + " AND " + q_filter[0] + " = ? "
+            q_list.append(q_filter[1])
+        res = BotState.DBLink.execute(get_q, tuple(q_list))
+        rows = res.fetchall()
+        BotState.DBLink.execute(del_q, tuple(q_list))
+        events = []
+        for row in rows:
+            event = ScheduledEvent(event_type, row[0], row[1],row[2:])
+            events.append(event)
+        BotState.write()
+        return events
+
+    @staticmethod
     def fetch_events(event_type: str,chat_id:int = 0, event_time: float = -1, filters=None):
         """
         Retrieves and removes events that have been scheduled before current time.
