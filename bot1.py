5003fbf0b53868a328ce9c8362eee36df3c6a1f0
Valentina <34088724+htmlcoderexe@users.noreply.github.com>
2026-06-26 20:29:41 +0200

    separate between user leaves and bans

--- bot1.py
+++ bot1.py
@@ -225,8 +225,11 @@ async def join_leave(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
             # user got removed?
             # #TODO: special event trigger for this
             new_user.log_event("removed",actor_uid)
+            if update.chat_member.new_chat_member.status == ChatMember.BANNED:
+                scheduled_events.ScheduledEvent.schedule_event("user_banned", chatid, -1, uid, actor_uid)
 
-        scheduled_events.ScheduledEvent.schedule_event("user_leave",chatid, -1, uid)
+        else:
+            scheduled_events.ScheduledEvent.schedule_event("user_leave",chatid, -1, uid)
         pass
     if new and not old:
         # user joined
